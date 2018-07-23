1f03a8d35681f875c12571a02c528c31bc0d36cf
oranc <oranc360@gmail.com>
2018-07-23 14:47:19 +0300

    little change - name classes

--- app/src/main/java/com/example/oran/exersicenetwork/views/SearchActivity.java
+++ app/src/main/java/com/example/oran/exersicenetwork/views/SearchActivity.java
@@ -36,7 +36,7 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
 
         findViewById();
         initRetrofit();
